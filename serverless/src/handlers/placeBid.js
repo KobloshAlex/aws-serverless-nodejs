e3b1f31d7640a82ba16b390e2232d9e7517e7ccb
Alex <Koblosh.Alex@gmail.com>
2020-12-07 19:47:08 -0500

    auth service

--- serverless/src/handlers/placeBid.js
+++ serverless/src/handlers/placeBid.js
@@ -10,13 +10,26 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 async function placeBid(event, context) {
 	const { id } = event.pathParameters;
 	const { amount } = event.body;
+	const { email } = event.requestContext.authorizer;
 
 	const auction = await getAuctionById(id);
 
+	//Bit identity validation
+	if (email === auction.seller) {
+		throw new createError.Forbidden(`Cannot bit on your own auction`);
+	}
+
+	// Avoid double bidding
+	if (email === auction.highestBid.bidder) {
+		throw new createError.Forbidden(`You are allready the highest bidder`);
+	}
+
+	// Auction status validate
 	if (auction.status !== "OPEN") {
 		throw new createError.Forbidden(`You cannon bid on closed auctions`);
 	}
 
+	// Bit amount validation
 	if (amount <= auction.highestBid.amount) {
 		throw new createError.InternalServerError(
 			`Your bid must be higher than ${auction.highestBid.amount}`
@@ -26,9 +39,11 @@ async function placeBid(event, context) {
 	const params = {
 		TableName: process.env.AUCTIONS_TABLE_NAME,
 		Key: { id },
-		UpdateExpression: " set highestBid.amount = :amount",
+		UpdateExpression:
+			" set highestBid.amount = :amount, highestBid.bidder = :bidder",
 		ExpressionAttributeValues: {
 			":amount": amount,
+			":bidder": email,
 		},
 		ReturnValues: "ALL_NEW",
 	};
