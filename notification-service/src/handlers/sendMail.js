3d0ecd541ec075959baeb5b1810a12a7508cfe69
Alex <Koblosh.Alex@gmail.com>
2020-12-07 21:05:52 -0500

    progress

--- notification-service/src/handlers/sendMail.js
+++ notification-service/src/handlers/sendMail.js
@@ -3,19 +3,25 @@ import AWS from "aws-sdk";
 const ses = new AWS.SES({ region: "eu-west-1" });
 
 async function sendMail(event, context) {
+	const record = event.Records[0];
+	console.log("record processing", record);
+
+	const email = JSON.parse(record.body);
+	const { subject, body, recipient } = email;
+
 	const params = {
 		Source: "koblosh.alex@gmail.com",
 		Destination: {
-			ToAddresses: ["koblosh.alex@gmail.com"],
+			ToAddresses: [recipient],
 		},
 		Message: {
 			Body: {
 				Text: {
-					Data: "Hello from AWS SES!",
+					Data: body,
 				},
 			},
 			Subject: {
-				Data: "Test Mail",
+				Data: subject,
 			},
 		},
 	};
